15afce83b2a409bd139045c83a6f2858516f7285
LutsenkoAnton <lutsenko.anton@icloud.com>
2024-01-25 18:59:05 +0300

    Add comments to SAT and modified solve()

--- sat.cpp
+++ sat.cpp
@@ -4,5 +4,17 @@
 void SAT::solve() {
     Model model;
     const CpSolverResponse response = SolveCpModel(cp_model.Build(), &model);
-    cout << CpSolverResponseStats(response) << endl;
+    if (response.status() == CpSolverStatus::OPTIMAL || response.status() == CpSolverStatus::FEASIBLE) {
+        for (const auto& vec1 : variables) {
+            for (const auto& vec2 : vec1) {
+                for (BoolVar var : vec2) {
+                    cout << var.Name() << " = " << SolutionBooleanValue(response, var) << '\n';
+                }
+                cout << '\n';
+            }
+            cout << "==================\n";
+        }
+    } else {
+        cout << "Solution not found (((((((" << endl;
+    }
 }
--- sat.h
+++ sat.h
@@ -7,9 +7,14 @@ using namespace std;
 
 class SAT {
 public:
+    // Creates SAT by Game. After this constructor SAT should already have all X_ijk variables created and all triangle restrictions should be added.
+    // Don't forget to name the variables
     SAT(const Game&);
+    // Adds strategy restriction to the SAT
     void add_strategy(const Strategy&, const Game&);
+    // Solves the SAT and prints the results
     void solve();
 private:
     CpModelBuilder cp_model;
+    vector<vector<vector<BoolVar>>> variables;
 };
