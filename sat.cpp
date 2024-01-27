b7b9d43db0aa805babdfc92a174ab031928f4264
Peskin Maksim <mppeskin_1@edu.hse.ru>
2024-01-27 15:57:00 +0300

    triangles added... Or should I say REMOVED?

--- sat.cpp
+++ sat.cpp
@@ -1,7 +1,7 @@
 #include "sat.h"
 #include <format>
 
-SAT::SAT(const Game& game) {
+SAT::SAT(const Game &game) {
     variables.resize(game.get_components_count());
     for (int i = 0; i < game.get_components_count(); ++i) {
         variables[i].resize(game.get_components_count());
@@ -17,20 +17,44 @@ SAT::SAT(const Game& game) {
         }
         for (int j = i + 1; j < terminals.size(); ++j) {
             for (int k = 0; k < game.get_player_count(); ++k) {
-                variables[terminals[i]][terminals[j]][k] = cp_model.NewBoolVar().WithName(format("X_{}_{}_{}", terminals[i], terminals[j], k));
+                variables[terminals[i]][terminals[j]][k] = cp_model.NewBoolVar().WithName(
+                        format("X_{}_{}_{}", terminals[i], terminals[j], k));
             }
         }
     }
     // Add triangle restrictions here
+    for (int player = 0; player < game.get_player_count(); ++player) {
+        for (int first = 0; first < terminals.size(); ++first) {
+            for (int second = 0; second < first; ++second) {
+                for (int third = 0; third < second; ++third) {
+                    int first_ter = terminals[first];
+                    int second_ter = terminals[second];
+                    int third_ter = terminals[third];
+                    // a bit of a kostil here, i should have checked, that one of the edges is backwards,
+                    // but we have triangles in both directions, so i can check, that one of them is in the right direction
+                    vector<BoolVar> triangle;
+                    triangle.push_back(get_var(first_ter, second_ter, player));
+                    triangle.push_back(get_var(second_ter, third_ter, player));
+                    triangle.push_back(get_var(third_ter, first_ter, player));
+                    vector<BoolVar> backwards_triangle;
+                    backwards_triangle.push_back(get_var(first_ter, second_ter, player).Not());
+                    backwards_triangle.push_back(get_var(second_ter, third_ter, player).Not());
+                    backwards_triangle.push_back(get_var(third_ter, first_ter, player).Not());
+                    cp_model.AddBoolOr(triangle);
+                    cp_model.AddBoolOr(backwards_triangle);
+                }
+            }
+        }
+    }
 }
 
 void SAT::solve() {
     Model model;
     const CpSolverResponse response = SolveCpModel(cp_model.Build(), &model);
     if (response.status() == CpSolverStatus::OPTIMAL || response.status() == CpSolverStatus::FEASIBLE) {
-        for (const auto& vec1 : variables) {
-            for (const auto& vec2 : vec1) {
-                for (optional<BoolVar> var : vec2) {
+        for (const auto &vec1: variables) {
+            for (const auto &vec2: vec1) {
+                for (optional<BoolVar> var: vec2) {
                     if (var) {
                         cout << var.value().Name() << " = " << SolutionBooleanValue(response, var.value()) << '\n';
                     }
@@ -56,7 +80,7 @@ void SAT::print_results() {
         for (int i = 0; i < variables.size(); ++i) {
             bool found = false;
             int cnt_better = 0;
-            for (const auto& vec2 : variables[i]) {
+            for (const auto &vec2: variables[i]) {
                 if (vec2[player]) {
                     found = true;
                     cnt_better += SolutionBooleanValue(response, vec2[player].value());
@@ -66,7 +90,7 @@ void SAT::print_results() {
             order[cnt_better] = i;
         }
         cout << "Order for player " << player << ": ";
-        for (auto elem : order) {
+        for (auto elem: order) {
             cout << elem << ' ';
         }
         cout << '\n';
@@ -81,11 +105,11 @@ BoolVar SAT::get_var(int i, int j, int k) {
     }
 }
 
-void SAT::add_strategy(const Strategy& strat, const Game& game) {
+void SAT::add_strategy(const Strategy &strat, const Game &game) {
     int outcome = game.play_strat(strat);
     for (int k = 0; k < game.get_player_count(); ++k) {
         vector<BoolVar> or_clause;
-        for (const auto& neighbour_strat : game.neighbour_strategies(strat, k)) {
+        for (const auto &neighbour_strat: game.neighbour_strategies(strat, k)) {
             int other_outcome = game.play_strat(neighbour_strat);
             if (other_outcome == outcome) continue;
             or_clause.push_back(get_var(other_outcome, outcome, k));
@@ -94,8 +118,8 @@ void SAT::add_strategy(const Strategy& strat, const Game& game) {
     }
 }
 
-void SAT::add_all_strategies(const Game& game) {
-    for (const auto& strat : game.generate_strategies()) {
+void SAT::add_all_strategies(const Game &game) {
+    for (const auto &strat: game.generate_strategies()) {
         add_strategy(strat, game);
     }
 }
